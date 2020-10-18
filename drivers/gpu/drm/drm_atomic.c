e5498054c939f1a9a712bcbd2193ed4cd59237a9
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-10-18 12:58:07 +0530

    drm: atomic/sde_crtc: Do not boost CPU/DDR after input timeout
     * this prevents cpu or ddr bus from boosting unnecessarily during video playback
       and similar usecases where a CPU boost is unnecessary during a frame commit
    
     * the default timeout of 3250 ms has been empirically determined by kdrag0n by
       performing multiple flings at various speeds while timing them. he observed
       the maximum fling time to be approximately 2850 ms, so our frame boost timeout
       of 3250 ms offers some headroom for more intensive applications such as games.

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -1904,6 +1904,10 @@ int drm_mode_atomic_ioctl(struct drm_device *dev,
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
 
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY))
+		if (df_boost_within_input(3250))
+			devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+
 	drm_modeset_acquire_init(&ctx, 0);
 
 	state = drm_atomic_state_alloc(dev);
